232b178d1d29b869075155ec409c5af928893175
André Taiar <taiar@hotmail.com>
2010-11-24 16:32:57 -0200

    gerador de texto randomico

---
+++ src/entrada/randTxt.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define PALMAXCHARS 15
+#define TAMANHOALFABETO 26
+#define QUANTPALAVRAS 100000000
+
+int main()
+{
+  int i, j;
+  int tamPal;
+  char txt[30];
+  char term[PALMAXCHARS + 5];
+  
+  srand(time(NULL));
+  strcpy(txt, "abcdefghijklmnopqrstuvwxyz");
+  
+  for(i = 0; i < QUANTPALAVRAS; i += 1)
+  {
+    tamPal = rand()%PALMAXCHARS;
+    tamPal = (tamPal<=1)?2:tamPal;
+    
+    for(j = 0; j < tamPal; j += 1)
+      term[j] = txt[rand()%TAMANHOALFABETO];
+    
+    term[tamPal] = '\0';
+    printf("%s", term);
+    
+    if((rand()%100)>=90) printf(". ");
+    else if((rand()%100)>=99) printf(".\n\n");
+    else printf(" ");
+  }
+  return 0;
+}
+
