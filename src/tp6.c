37423872e84627aea249b2547d91ef7fb9f2e3c2
André Taiar <taiar@hotmail.com>
2010-11-27 15:06:31 -0200

    teste

---
+++ src/tp6.c
@@ -0,0 +1,20 @@
+/*
+ * tp6.c
+ *
+ *  Created on: 23/11/2010
+ *      Author: taiar
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+
+#include "io.h"
+
+int main(int argc, char **argv)
+{
+
+  return EXIT_SUCCESS;
+}
